8a104e1d355029401a7b3628a3e1226395b55666
Max Winderbaum <maxwinderbaum@gmail.com>
2019-11-25 20:43:05 -0800

    Clap 👏 in 👏 a 👏 tor 👏

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import copy from 'copy-to-clipboard';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 function App() {
+  const [textToClapinate, setTextToClapinate] = useState('Clap in a tor');
+  const [notifyOpen, setNotifyOpen] = React.useState(false);
+
+  const showCopySuccess = () => {
+    setNotifyOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setNotifyOpen(false);
+  };
+
+  const handleTextInput = (event) => {
+    setTextToClapinate(event.target.value);
+  };
+  const handleFocus = (event) => event.target.select();
+
+  const clapinatedText = textToClapinate.trim().concat(' ').split(/\s/).join(` 👏 `);
+  const handleCopy = () => {
+    copy(clapinatedText);
+    showCopySuccess();
+  };
+
+  const textStyle = {
+    margin: '0 auto',
+    marginTop: 10,
+    maxWidth: 500,
+    minHeight: 200,
+    fontSize: 40,
+    padding: 30,
+  };
+
+  const buttonStyle = {
+    marginTop: 7,
+    marginLeft: 20,
+  };
+
+  const snackbarStyle = {
+    backgroundColor: 'green',
+    borderRadius: 10,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField value={textToClapinate} onFocus={handleFocus} onChange={handleTextInput} id="outlined-basic" label="Text to Clapinate" variant="outlined" />
+        <Button onClick={handleCopy} style={buttonStyle} size="large" variant="contained" color="primary">Copy</Button>
+      </div>
+      <div style={textStyle}>
+        {clapinatedText}
+      </div>
+      <Snackbar
+        style={snackbarStyle}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={notifyOpen}
+        onClose={handleClose}
+        autoHideDuration={2000}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+      >
+        <SnackbarContent message={<span id="message-id">Copied to clipboard!</span>} style={snackbarStyle}></SnackbarContent>
+      </Snackbar>
     </div>
   );
 }
